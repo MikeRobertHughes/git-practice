0de4e4476e64ebcb7d26cd08afc039d85ba80e00
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-08-25 14:34:27 -0500

    Finish bouncer.rb in Hard Mode

--- bouncer.rb
+++ bouncer.rb
@@ -1,23 +1,54 @@
 ########################## Normal Mode ##########################
 
+# def check_age
+
+#   print "What is your age? > "
+#   age = gets.chomp.to_i
+
+#   if age < 18
+#     puts "You can do nothing!"
+#   elsif age >= 18
+#     puts "You can vote."
+#     puts "You can smoke."
+#     if age >= 21
+#       puts "You can drink."
+#       if age >= 25
+#         puts "You can rent a car."
+#       end
+#     end
+#   else
+#     puts "This line may never print."
+#   end
+# end
+
+# check_age
+
+########################## Hard Mode ##########################
+
 def check_age
 
-  print "What is your age? > "
-  age = gets.chomp.to_i
+  loop do
+    print "What is your age? > "
+    age = gets.chomp
+    age_num = age.to_i
 
-  if age < 18
-    puts "You can do nothing!"
-  elsif age >= 18
-    puts "You can vote."
-    puts "You can smoke."
-    if age >= 21
-      puts "You can drink."
-      if age >= 25
-        puts "You can rent a car."
+    if age.upcase == 'Q'
+      puts "Exiting..."
+      exit
+    elsif age_num >= 105
+      puts "You're probably a ghost."
+    elsif age_num < 18
+      puts "You can do nothing!"
+    elsif age_num >= 18
+      puts "You can vote."
+      puts "You can smoke."
+      if age_num >= 21
+        puts "You can drink."
+        if age_num >= 25
+          puts "You can rent a car."
+        end
       end
     end
-  else
-    puts "This line may never print."
   end
 end
 
